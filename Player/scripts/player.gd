8cd25f61ebaa01452a4fda76593b84c7ad13e77e
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-01-11 14:30:46 +0100

    Added state machine (lowk I don't understand this)

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -2,30 +2,23 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 var direction : Vector2 = Vector2.ZERO
-var move_speed : float = 100.0
-var state : String = "idle"
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var state_machine: PLayerStateMachine = $StateMachine
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	state_machine.Initialize(self)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
-	velocity = direction * move_speed
-	
-	if SetState() == true || SetDirection() == true:
-		UpdateAnimation()
-	
 	pass
 
 	
@@ -50,19 +43,10 @@ func SetDirection() -> bool:
 	cardinal_direction = new_dir
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
-	
-	
 
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-	
-	
 
-func UpdateAnimation() -> void:
+
+func UpdateAnimation( state : String) -> void:
 	animation_player.play(state + "_" + AnimDirection())
 	pass
 
